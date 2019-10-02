e3d461a4985ca432337f2515720e35469501a5e6
Ran <ran.guin@gmail.com>
2019-10-01 22:24:33 -0700

    move standard views to pages dir

---
+++ client/src/layouts/Footer.vue
@@ -0,0 +1,22 @@
+<template lang='pug'>
+  div.insideFooter()
+    b Social Platform Affecting Real Connections
+    Logo(style='float: right')
+</template>
+
+<script>
+import Logo from '@/components/Standard/Logo'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+<style scoped>
+.insideFooter {
+  display: flex row;
+  width: 100%;
+  display: inline-block;
+}
+</style>
---
+++ client/src/layouts/Header.vue
@@ -0,0 +1,42 @@
+<template lang='pug'>
+  v-app-bar(app)
+    Logo(style='flex: 1')
+    span(style='flex:3') Connecting Communities One Event at a Time...
+    v-spacer
+    v-tabs(style='flex:2' right hide-slider)
+      v-tab(v-if='link.public' v-for='link in headerLinks')
+        router-link(:to='link.target || link.name')
+          v-btn.btn-primary {{link.name}}
+</template>
+
+<script>
+import Logo from '@/components/Standard/Logo'
+
+export default {
+  data () {
+    return {
+      headerLinks: [
+        {name: 'About', public: true},
+        {name: 'Login', public: true},
+        {name: 'Logout'},
+        {name: 'Admin', public: true, target: 'admin'},
+        {name: 'Data', public: true},
+        {name: 'Home', public: true}
+      ]
+    }
+  },
+  components: {
+    Logo
+  },
+  computed: {
+    payload: function () {
+      return null
+    }
+  }
+}
+</script>
+<style>
+.v-toolbar__content {
+  align-items: center;
+}
+</style>
