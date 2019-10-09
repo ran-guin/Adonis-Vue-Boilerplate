0cdc70a45bfa0b69ae045e176b890dda09f75a51
Ran <ran.guin@gmail.com>
2019-10-08 20:21:06 -0700

    separate public & private header buttons

--- client/src/layouts/Header.vue
+++ client/src/layouts/Header.vue
@@ -4,8 +4,8 @@
     span(style='flex:3') Connecting Communities One Event at a Time...
     v-spacer
     v-tabs(style='flex:2' right hide-slider)
-      v-tab(v-if='link.public' v-for='link in headerLinks' :key='link.name')
-        router-link(:to='link.target || link.name')
+      v-tab(v-for='link in headerLinks' :key='link.name')
+        router-link(v-if='visible(link)' :to='link.target || link.name')
           v-btn.btn-primary {{link.name}}
 </template>
 
@@ -15,13 +15,15 @@ import Logo from '@/components/Standard/Logo'
 export default {
   data () {
     return {
-      headerLinks: [
+      publicHeaders: [
         {name: 'About', public: true},
-        {name: 'Login', public: true},
-        {name: 'Logout'},
-        {name: 'Admin', public: true, target: 'admin'},
-        {name: 'Data', public: true},
-        {name: 'Home', public: true}
+        {name: 'Login', public: true}
+      ],
+      privateHeaders: [
+        {name: 'Logout', private: true},
+        {name: 'Admin', private: true, access: 'admin', target: 'admin'},
+        {name: 'Data', private: true},
+        {name: 'Home', private: true}
       ]
     }
   },
@@ -29,8 +31,24 @@ export default {
     Logo
   },
   computed: {
+    headerLinks: function () {
+      if (this.payload && this.payload.userid) {
+        return this.privateHeaders
+      } else {
+        return this.publicHeaders
+      }
+    },
     payload: function () {
-      return null
+      return this.$store.getters.payload || {}
+    }
+  },
+  methods: {
+    visible: function (link) {
+      if (link.access === 'admin') {
+        return (this.payload.access === 'admin')
+      } else {
+        return true
+      }
     }
   }
 }
