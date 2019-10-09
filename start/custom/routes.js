173c041c51ffa17c791092e0dd21f3be8c2700ac
Ran <ran.guin@gmail.com>
2019-10-08 20:17:33 -0700

    tidy up routes

--- start/custom/routes.js
+++ start/custom/routes.js
@@ -1,7 +1,9 @@
 const Route = use('Route')
 
 Route.on('/').render('welcome')
-Route.get('/dump', 'DatumController.dump')
-Route.get('/view', 'DatumController.dataview')
-Route.get('/vuetify', 'DatumController.vuetify')
+
+Route.get('/dataset', 'DatumController.query')
+Route.put('/dataset/:id', 'DatumController.update')
+Route.post('/dataset', 'DatumController.append')
+Route.delete('/dataset/:id', 'DatumController.delete')
 
--- start/routes.js
+++ start/routes.js
@@ -22,90 +22,47 @@ const custom_routes = require('./custom/routes.js')
 
 console.log('load routes..')
 
-// SSR pages
-Route.get('/', 'PublicController.test')
+// Public SSR pages
+Route.get('/', 'PublicController.home')
 Route.get('/test', 'PublicController.test')
-
-Route.get('', 'PublicController.landingPage')
-Route.get('/', 'PublicController.landingPage')
-
-Route.get('web', 'PublicController.landingPage')
-Route.get('/web/:target', 'PublicController.landingPage')
-Route.get('public', 'PublicController.landingPage')
-Route.get('public/:target', 'PublicController.landingPage')
-
-Route.get('public', 'PublicController.landingPage')
-Route.get('public/:target', 'PublicController.landingPage')
-
-Route.get('web', 'PublicController.landingPage')
-Route.get('web/:target', 'PublicController.landingPage')
-
-Route.get('testPage', 'TestController.testPage')
-Route.get('testPage/:version', 'TestController.testPage')
-
-// Footer Pages
-Route.get('faqs', 'PublicController.faqs')
-Route.get('faqs/:faq', 'PublicController.faqs')
-
-Route.get('team', 'PublicController.team')
-Route.get('team/:member', 'PublicController.team')
-
-Route.get('contactUs', 'PublicController.contactUs')
-Route.get('contactUs/:from', 'PublicController.contactUs')
-
-Route.get('investors', 'PublicController.investors')
 
 Route.get('login', 'PublicController.login')
 Route.get('register', 'PublicController.register')
-
+Route.get('recover', 'PublicController.recoverPassword')
 Route.get('construction', 'PublicController.construction')
 
-// API access
+Route.post('/test', 'PublicController.register')
+Route.post('/login', 'AuthController.login')
+Route.post('logout', 'AuthController.logout')
+Route.post('register', 'AuthController.register')
+
+Route.get('/check', 'AuthController.check')
+
+// Auth access
 Route.get('/env', 'AuthController.env')
-Route.get('/recoverPassword', 'AuthController.recoverPassword')
-Route.get('/resetPassword', 'AuthController.resetPassword')
 Route.get('/accessResetPassword', 'AuthController.accessResetPassword')
-Route.get('/accessResetPassword/:token', 'AuthController.accessResetPassword')
-Route.get('/confirmRegistration/:token', 'AuthController.confirmRegistration')
 Route.get('/resendWelcome', 'AuthController.resendWelcome')
 
-Route.post('/env', 'AuthController.env')
 Route.post('/recoverPassword', 'AuthController.recoverPassword')
-Route.post('/accessResetPassword', 'AuthController.accessResetPassword')
-Route.post('/accessResetPassword/:token', 'AuthController.accessResetPassword')
 Route.post('/resetPassword', 'AuthController.resetPassword')
 Route.post('/confirmRegistration', 'AuthController.confirmRegistration')
 
 Route.post('/message', 'PublicController.message')
-Route.get('/message', 'PublicController.message')
 
 Route.get('/docs', 'HelpController.viewDoc')
-Route.post('/docs', 'HelpController.viewDoc')
 
 // Public Routes:
 
-Route.get('recover', 'PublicController.recoverPassword')
-Route.post('recover', 'PublicController.recoverPassword')
-
-Route.post('login', 'AuthController.login')
-
-// Route.get('/logout', 'AuthController.logout')
-Route.post('logout', 'AuthController.logout')
-
-// Route.get('/register', 'AuthController.register')
-Route.post('register', 'AuthController.register')
-
-Route.get('/check', 'AuthController.check')
-Route.post('/check', 'AuthController.check')
 
 // Generic routes
 Route.get('/loadTable', 'DBFormController.loadTable')
 Route.get('/loadTable/:table', 'DBFormController.loadTable')
-Route.post('/loadTable', 'DBFormController.loadTable')
 Route.post('/saveForm', 'DBFormController.saveRecord')
 
 // Generic routes
-Route.get('/search', 'GenericController.search')
-Route.get('/getData', 'GenericController.search')
-Route.get('/describe', 'GenericController.describe')
-
+Route.get('/tables', 'GenericController.tables')
+Route.get('/tables/:table', 'GenericController.describe')
+Route.get('/dataset/:table', 'GenericController.search')
+Route.post('/dataset/:table', 'GenericController.append')
+Route.put('/dataset/:table/:id', 'GenericController.update')
+Route.delete('/dataset/:table/:id', 'GenericController.delete')
