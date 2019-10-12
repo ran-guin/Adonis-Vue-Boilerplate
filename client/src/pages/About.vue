197046c7722f06298d5528e95d26b25905be6f63
Ran Guin <rguin@iMac.local>
2019-10-11 20:19:11 -0700

    enable crud in data page

--- client/src/pages/About.vue
+++ client/src/pages/About.vue
@@ -1,19 +1,46 @@
 <template lang='pug'>
   PageLayout()
-    v-container
-      h1 This is a boilerplate for Adonis + View with:
+    v-container(app dark)
+      h1 About
+      h3 This is a boilerplate for Adonis + View with:
       ul
         li
           router-link(to='Login') Login
         li
           router-link(to='Search') Data Search
+      hr
+      img.logo(src='@/assets/images/sine.svg' height='300px')
+      hr
+      p &nbsp;
+      Logo
+
 </template>
 
 <script>  
 import PageLayout from '@/layouts/PageLayout'
+import Logo from '@/components/Logo'
+
 export default {
   components: {
-    PageLayout
+    PageLayout,
+    Logo
+  },
+  methods: {
+    dval: function (offset) {
+      if (offset === 10) {
+        return "M10 80 C 40 10, 65 10, 95 80 S 150 150, 180 80"
+      } else {
+        return "M90 80 C 120 10, 145 10, 175 80 S 230 150, 260 80"
+      }
+    }
   }
 }
 </script>
+<style>
+  .small { font: italic 13px sans-serif; }
+  .heavy { font: bold 30px sans-serif; }
+
+  /* Note that the color of the text is set with the    *
+    * fill property, the color property is for HTML only */
+  .Rrrrr { font: italic 40px serif; fill: red; }
+</style>
--- client/src/pages/Data.vue
+++ client/src/pages/Data.vue
@@ -1,23 +1,49 @@
 <template lang='pug'>
     PageLayout()
-        v-container
-            v-btn.btn-primary(@click='loadTable') Load Table
-            v-text-field(v-model='fieldList')
-            v-btn.btn-primary(@click='loadData') Load Data
+        v-container(light)
+            v-text-field(light v-model='table' style='background-color: white; padding: 0.5rem;' label='Database Table')
+            v-text-field(light v-model='condition' style='background-color: white; padding: 0.5rem;' label='Condition')
+            div(style='display: inline-block')
+                v-btn.btn-primary(v-if='table' @click='loadTable') Load Table
+                span &nbsp; &nbsp;
+                v-btn.btn-primary(v-if='table' @click='loadData') Load Data
         hr
-        v-container
-            v-tabs(model='tab')
+        v-container(fluid v-if='dataFields')
+            v-checkbox(v-for='fld in dataFields' v-model='pickFields' :label='fld' :value='fld' :key='fld' @change='reRenderTable')
+        v-container(dark)
+            v-tabs(model='tab' hide-slider)
                 v-tab(href='#tab-0') Data
                 v-tab(href='#tab-1') Graph
                 v-tab(href='#tab-2') Dump
                 hr
                 v-tab-item(value='tab-0')
-                    v-data-table(v-if='dataset' :headers='showFields' :items='dataset' :items-per-page='20' class='elevation-9')
+                    v-data-table(v-if='dataset' :headers='headers' :items='dataset' :items-per-page='20' class='elevation-9')
+                        template(v-slot:top)
+                            v-dialog(v-model="dialog" max-width="500px")
+                                template(v-slot:activator="{ on }")
+                                    v-btn(color="primary" dark class="mb-2" v-on="on") New Item
+                                v-card
+                                    v-card-title Dataset:
+                                    v-card-text
+                                        v-container
+                                            v-row
+                                                v-col(cols="12" sm="6" md="4")
+                                                    v-text-field(v-for='fld in dataFields' v-model="form[fld]" :label="fld" :key='fld')
+                                    v-card-actions
+                                        div.flex-grow-1
+                                            v-btn(color="blue darken-1" text @click="close") Cancel
+                                            v-btn(color="blue darken-1" text @click="save") Save
+                        template(v-slot:item.action="{ item }")
+                            v-icon.mr-2(small @click='editItem(item)') edit
+                            v-icon.mr-2(small @click='deleteItem(item)') delete
+                        template(v-slot:no-data)
+                            v-btn(color='primary' @click='initialize') Reset
                 v-tab-item(value='tab-1')
                     h3 Graph...:
                 v-tab-item(value='tab-2')
                     h3 Dump:
-                    h4 Fields: {{JSON.stringify(showFields, null, 2)}}
+                    h4 Fields: {{JSON.stringify(dataFields, null, 2)}}
+                    h4 Headers: {{JSON.stringify(headers, null, 2)}}
                     h4 Dataset: {{JSON.stringify(dataset, null, 2)}}
             hr
 </template>
@@ -35,16 +61,24 @@ export default {
         return {
             fieldList: 'username, email',
             dataset: [],
-            showFields: [],
+            table: 'users',
+            condition: '',
+            dataFields: [],
+            headers: [],
+            pickFields: [],
             apiURL: config.apiURL[process.env.NODE_ENV],
-            tableFields: ['Field', 'Type', 'Null', 'Key', 'Default']
+            tableFields: ['Field', 'Type', 'Null', 'Key', 'Default'],
+            crud: true,
+            editedIndex: -1,
+            form: {},
+            dialog: false
         }
     },
     created: function () {
         console.log('Data viewer')
         if (this.data) {
             this.dataset = this.data
-            this.showFields = this.fields
+            this.dataFields = this.fields
         }
     },
     props: {
@@ -52,9 +86,15 @@ export default {
         fields: { type: Array }
     },
     methods: {
+        initialize: function () {
+            this.dataset = []
+            this.pickFields = []
+            this.dataFields = []
+            this.headers = []
+        },
         loadTable: function () {
-            const url = this.apiURL + '/tables/users/'
-            console.log('get data from ' + url)
+            const url = this.apiURL + '/tables/' + this.table
+            console.log('desc table from ' + url)
             const _this = this
             axios.get(url)
                 .then(function (response) {
@@ -66,7 +106,10 @@ export default {
                 })
         },
         loadData: function () {
-            const url = this.apiURL + '/dataset/users'
+            var url = this.apiURL + '/dataset/' + this.table
+
+            if (this.condition) { url = url + '?condition=' + this.condition }
+
             console.log('get data from ' + url)
             const _this = this
             axios.get(url)
@@ -90,10 +133,65 @@ export default {
         },
 
         parseFields: function (fields) {
-            this.showFields = []
+            this.dataFields = []
+            this.headers = []
             for (var i = 0; i < fields.length; i++) {
-                this.$set(this.showFields, i, { text: fields[i], value: fields[i] })
+                this.$set(this.headers, i, { text: fields[i], value: fields[i] })
+                this.dataFields.push(fields[i])
             }
+            if (this.crud && fields.length) {
+                this.headers.push({ text: 'Actions', value: 'action', sortable: false })
+            }
+            this.pickFields = this.dataFields
+        },
+
+        reRenderTable: function () {
+            this.headers = []
+            var fields = this.pickFields;
+            for (var i = 0; i < fields.length; i++) {
+                this.$set(this.headers, i, { text: fields[i], value: fields[i] })
+            }
+
+            if (this.crud && fields.length) {
+                this.headers.push({ text: 'Actions', value: 'action', sortable: false })
+            }
+
+        },
+        editItem (item) {
+            this.editedIndex = this.dataset.indexOf(item)
+            console.log('edit item ' + this.editedIndex)
+            console.log(JSON.stringify(this.dataset[this.editedIndex]))
+
+            for (var i = 0; i < this.dataFields.length; i++) {
+                var fld = this.dataFields[i]
+                this.$set(this.form, fld, this.dataset[this.editedIndex][fld])
+            }
+            console.log('Form: ' + JSON.stringify(this.form))
+            this.editedItem = Object.assign({}, item)
+            this.dialog = true
+        },
+
+        deleteItem (item) {
+            const index = this.dataset.indexOf(item)
+            console.log('Delete: ' + JSON.stringify(this.dataset[index]))
+            confirm('Are you sure you want to delete this item?') && this.dataset.splice(index, 1)
+        },
+
+        close () {
+            this.dialog = false
+            setTimeout(() => {
+                this.editedItem = Object.assign({}, this.defaultItem)
+                this.editedIndex = -1
+            }, 300)
+        },
+
+        save () {
+            if (this.editedIndex > -1) {
+                Object.assign(this.dataset[this.editedIndex], this.editedItem)
+            } else {
+                this.dataset.push(this.editedItem)
+            }
+            this.close()
         }
     }
 }
