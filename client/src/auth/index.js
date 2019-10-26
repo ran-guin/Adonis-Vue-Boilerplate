5ae083a006af7874e33193e9ddc170075ac0dbc9
Ran Guin <rguin@iMac.local>
2019-10-26 12:08:14 -0700

    update auth with idvpn updates

--- client/src/auth/index.js
+++ client/src/auth/index.js
@@ -13,72 +13,85 @@ export default {
     authenticated: false
   },
   // call api to login and return token
-  login (context, creds, redirect) {
-    console.log('get auth.index.login for ' + process.env.NODE_ENV + ' : ' + apiURL)
-    if (redirect) { console.log('redirect to: ' + redirect) }
+  login (context, creds) {
+    console.debug('get auth.index.login for ' + process.env.NODE_ENV + ' : ' + apiURL)
     return axios.post(apiURL + '/login', creds)
   },
   // call api to register new user
-  signup (context, creds, redirect) {
-    console.log('post auth.index.signup ' + apiURL)
-    console.log('redirect to: ' + redirect)
+  signup (context, creds) {
+    console.debug('post auth.index.signup ' + apiURL)
     return axios.post(apiURL + '/register', creds)
   },
 
-  validate () {
-    console.log('validate connection...')
+  async validate (root) {
+    console.debug('validate connection...' + apiURL)
+    var _this = this
     return axios.post(apiURL + '/check')
       .then(function (response) {
-        console.log('validate: ' + JSON.stringify(response))
+        console.debug('validate connection status: ' + JSON.stringify(response))
+        if (response && response.data && response.data.logged_in) {
+          console.debug('still logged in...')
+        } else {
+          _this.logout(root, 'expired')
+        }
         return response
       })
       .catch(function (err) {
-        console.log('validate error: ' + JSON.stringify(err))
+        console.debug('validate error: ' + JSON.stringify(err))
         return {error: err}
       })
   },
   refresh () {
-    console.log('refresh token in auth......')
+    console.debug('refresh token in auth......')
     const refreshToken = localStorage.getItem('refresh-token')
     var data = {refresh_token: refreshToken}
     if (refreshToken) {
-      console.log('POST: ' + apiURL + '/refreshToken')
-      console.log('data: ' + JSON.stringify(data))
+      console.debug('POST: ' + apiURL + '/refreshToken')
+      console.debug('data: ' + JSON.stringify(data))
       return axios.post(apiURL + '/refreshToken', data)
     } else {
-      console.log('no refresh token')
+      console.debug('no refresh token')
       return new Promise(function (resolve) {
         resolve({message: 'no refresh token'})
       })
     }
   },
   updateToken () {
-    console.log('update token if required')
+    console.debug('update token if required')
     const token = localStorage.getItem('user-token')
-    console.log('token: ' + token)
+    console.debug('token: ' + token)
     if (token) {
       var pass = 'Bearer ' + token
       axios.defaults.headers.common['Authorization'] = pass
-      console.log('using auth token: ' + pass)
+      console.debug('using auth token: ' + pass)
       return token
     } else {
-      console.log('no current token to update')
+      console.debug('no current token')
       return null
     }
   },
 
   // To log out, we just need to remove the token
-  logout (context, loginId) {
-    console.log('logging out...')
-    localStorage.removeItem('user-token')
-    this.user.authenticated = false
-
-    var data = {
-      login_id: loginId
+  logout (root, context) {
+    try {
+      var loginId
+      if (root.payload && root.payload.login_id) {
+        loginId = root.payload.login_id
+      }
+  
+      if (loginId) {
+        var data = {
+          login_id: loginId,
+          note: context
+        }
+        // location.href = apiURL
+        axios.post(apiURL + '/logout', data)
+        root.$store.dispatch('AUTH_LOGOUT')
+      }
+    } catch (err) {
+      console.log('logout error:' + err)
     }
-    location.href = apiURL
-    console.log('client side redirect to homepage')
-    return axios.post(apiURL + '/logout', data)
+    // this.$store.dispatch('AUTH_LOGOUT')
   },
   // just update local authenticated flag
   checkAuth () {
@@ -88,7 +101,7 @@ export default {
     // } else {
     //   this.user.authenticated = false
     // }
-    console.log('check authentication... (under construction)')
+    console.debug('check authentication... (under construction)')
   },
 
   // The object to be passed as a header for authenticated requests
