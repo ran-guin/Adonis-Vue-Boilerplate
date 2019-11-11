53f490cf61ebe7915a415145951182ef0b557c20
Ran Guin <ran.guin@gmail.com>
2019-11-10 20:57:12 -0800

    simplify construction page

--- client/src/pages/Construction.vue
+++ client/src/pages/Construction.vue
@@ -1,31 +1,8 @@
 <template lang='pug'>
   PageLayout
     div.info-header.full-width-padding
-      div.info-logo
-        a(v-on:click="$router.push('/Home')")
-          img(src='images/kyc/pgkyc-key.png' height='80px')
-      div.info-left
-        h4 A Pretty Good, privacy centric KYC solution
-        div(v-if="payload && (payload.role === 'Admin' || payload.role === 'Tester')")
-          b.env(:class='envClass') {{payload.codeVersion}} : DB = {{payload.db}}
-        div(v-else-if="payload && payload.env && payload.env !== 'production'")
-          b.env(:class='envClass') {{payload.env}} version
-      div.info-right
-        Authorization(:payload='payload' :add2Menu='add2Menu' :demo='demo' :noConfirm='!confirm' :mode='mode')
-        h5(v-if="role && payload.role !== 'User'") [as {{role}}]
-      hr.std-colour.wideScreen(style='border: 3px solid')
+      h4 Under Construction 
       p &nbsp;
-
-    div.myBody
-      div.myContainer
-        div.desktop-message
-          p &nbsp;
-          h2 Welcome to PGKYC!
-            p &nbsp;
-            p A privacy centric PKI based KYC solution on the blockchain
-          p &nbsp;
-
-    FooterLayout.myFooter()
 </template>
 
 <script>
