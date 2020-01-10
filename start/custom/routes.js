cb646f9f32a5996da3fe6c5871d1c171195c7f28
Ran <ran.guin@gmail.com>
2020-01-10 13:07:26 -0800

    remove custom routes file

--- start/custom/routes.js
+++
@@ -1,9 +0,0 @@
-const Route = use('Route')
-
-Route.on('/').render('welcome')
-
-Route.get('/dataset', 'DatumController.query')
-Route.put('/dataset/:id', 'DatumController.update')
-Route.post('/dataset', 'DatumController.append')
-Route.delete('/dataset/:id', 'DatumController.delete')
-
