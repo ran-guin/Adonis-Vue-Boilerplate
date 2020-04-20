139094cf289a6b24d2268811698b0592266c80d2
Ran Guin <rguin@iMac.local>
2020-04-20 16:42:46 -0700

    add apiAccess mixin

---
+++ client/src/default/mixins/apiAccess.vue
@@ -0,0 +1,76 @@
+ <!--
+
+Usage example.
+
+
+ -->
+
+<script>
+import Config from '@/config.js'
+import axios from 'axios'
+
+export default {
+    data () {
+        return {
+            apiURL: Config.apiURL[process.env.NODE_ENV],
+            debug: Config.DEBUG_MODE,
+            apiResponse: null,
+            apiResponseError: ''
+        }
+    },
+    mounted: function () {
+    
+    },
+    created: function () {
+        console.log("Including DataLoader mixin...")
+    },
+    computed: {
+        payload: function () {
+            return this.$store.getters.payload || {}
+        }
+    },
+    methods: {
+        loadLookup (table, reference) {
+            var url = this.apiURL + '/lookup/' + table
+            var lookup = this.formLookup || {}
+            console.log('url: ' + url)
+            var _this = this
+            return axios.get(url)
+                .then(function (response) {
+                    lookup[reference] = response.data
+                    _this.formLookup = lookup
+                })
+                .catch(function (err) {
+                    console.log("Error retrieving lookup: " + err.message)
+                })
+        },
+        apiCall (path, attribute) {
+            var url = this.apiURL + path
+            console.log('url: ' + url)
+            this.apiResponse = null
+            this.apiResponseError = ''
+            var _this = this
+            console.log('url: ' + url)
+            return axios.get(url)
+                .then(function (response) {
+                    console.log('got response: ' + JSON.stringify(response))
+                    if (response && response.data)
+                        if (attribute) {
+                            if (response.data[attribute]) {
+                                _this.apiResponse = response.data[attribute]
+                            } else {
+                                _this.apiResponseError = 'no ' + attribute + ' attribute in response'
+                            }
+                        } else {
+                            _this.apiResponse = response.data
+                        }
+                })
+                .catch(function (err) {
+                    console.log('Error with api call: ' + url)
+                    console.log(err.message)
+                    _this.apiResponseError = 'apiCall error: ' + err.message
+                })           
+        }
+    }
+}
+</script>
