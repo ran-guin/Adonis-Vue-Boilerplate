4e0e88ff207016af66beb2884e82e3edddc62614
Ran Guin <rguin@iMac.local>
2020-05-02 14:40:08 -0700

    add default custom config

--- config/custom.js
+++ config/custom.js
@@ -1,4 +1,39 @@
 module.exports = {
   invitation_required: false,
-  loginRequired: false
+  loginRequired: false,
+  lookup: {
+    public: [
+      'interests',
+      'services',
+      'goods'
+    ],
+    private: [
+      'users'
+    ]
+  },
+  payloadContent: {
+   user: {
+    userid: 'id',
+    username: 'username',
+    email: 'email',
+    access: 'access',
+    role: 'role',
+    status: 'status',
+    uuid: 'UUID'
+   },
+   login: {
+    login_id: 'id',
+    remoteAddress: 'ip'
+   },
+   settings: {
+    latitude: 'latitude',
+    longitude: 'longitude',
+    mobility: 'access_radius'
+   },
+   default: {
+    role: 'User',
+    uuid: 'tbd'
+   }
+  }
+
 }
