0de0ebedfe91dc890168dee9a7d19758f9d204d5
Ran Guin <rguin@iMac.local>
2020-07-11 14:38:22 -0700

    fix myString

--- client/src/services/myString.js
+++ client/src/services/myString.js
@@ -1,48 +1,73 @@
 export default {
-  zohoMail(Message) {
-    // Requires config for: EMAIL_SOURCE (eg info@cosinesystems.org), EMAIL_SOURCE_PASSWORD
-    var subject = Message.subject || 'Automated Message'
-    var text = Message.text
-    var html = Message.html
-    var from = Message.from || config.EMAIL_SOURCE
-    var to = Message.to
-    var mailPwd = config.EMAIL_SOURCE_PASSWORD
-    var from_alias = Message.alias || 'Cosine Systems'
-
-    if (!(to && from)) {
-      return Promise.reject(new Error('Missing source or target email address'));
-    } else if (!mailPwd) {
-      return Promise.reject(new Error('Email Password not defined'));
-    } else if (!(text || html)) {
-      return Promise.reject(new Error('Missing body'));
-    } else {
-      if (to && from (text || html)) {
-        var transporter = nodemailer.createTransport({
-          host: 'smtp.zoho.com',
-          port: 465,
-          secure: true, // use SSL
-          auth: {
-              user: from,
-              pass: mailPwd
-          }
-      });
-      
-      // setup e-mail data, even with unicode symbols
-      var mailOptions = {
-          from: '"' + from_alias + '" ' + from, // sender address (who sends)
-          to: to, // list of receivers (who receives)
-          subject: subject, // Subject line
-          text: text, // plaintext body
-          html: html // html body
-      };
-      
-      // send mail with defined transport object
-      transporter.sendMail(mailOptions, function(error, info){
-          if(error){
-              return Promise.reject(new Error(error));
-          } else {
-            return Promise.resolve('Message sent: ' + info.response)
-          } 
-      });      
+  camelCase(str) {
+     var parts = str.split(/[\s-_]/);
+     var words = [] 
+     if (parts.length) {
+       for (var i = 0; i < parts.length; i++) {
+         var word = ''
+         if (i > 0) {
+           word = parts[i].substr(0,1).toUpperCase();
+         } else {
+           word = parts[i].substring(0,1); // .toLowerCase();
+         }
+         if (parts[i].length > 1) {
+           word = word + parts[i].substring(1,parts[i].length); // .toLowerCase();
+         }
+         words.push(word)
+       }
+     } else {
+       words.push(str)
+     }
+     return words.join('');
+  },
+  normalize(str) {
+    var decamel = this.deCamelCase(str)
+    var split = decamel.split(/\s|_/)
+    return split.join(' ')
+  },
+  capitalize(str) {
+    var string = str
+    var words = this.normalize(str).match(/ [a-z]/)
+    if (words.length > 1) {
+      for (var j = 1; j < words.length; j++) {
+        string = string.replace(words[j], words[j].toUpper())
+      }
     }
+    return string
+  },
+  deCamelCase(str, sep) {
+    if (!sep) { sep = ' ' }
+
+    var camel = this.camelCase(str)
+    var normalized = camel.substr(0,1).toUpperCase();
+    if (camel.length > 1) {
+      normalized = normalized + camel.substr(1, camel.length - 1);
+    }
+    
+    var words = this.splitCamelCase(normalized)
+    return words.join(sep)
+  },
+  splitCamelCase(str, array, loops) {
+     if (!array) { array = [] }
+     if (!loops) { loops = 0 }
+     loops++
+
+     if (loops > 10) {
+       array.push(str)
+       return array
+     }
+    
+     var parts = str.match(/([A-Z][a-z]*)/);
+     if (parts) {
+       array.push(parts[1])
+       var remainder = str.replace(parts[1], '')
+       if (remainder) {
+         return this.splitCamelCase(remainder, array, loops)
+       } else {
+         return array
+       }
+     } else {
+       return array
+     }
   }
+}
