2bcd8c109e0d24516a53848a950b7236314166e3
Ran Guin <rguin@iMac.local>
2020-11-29 12:31:06 -0800

    tweak group model

--- app/Models/Group.js
+++ app/Models/Group.js
@@ -2,52 +2,38 @@
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
+const Database = use('Database')
 
 class Group extends Model {
 
-    async list (user_id) {
-        var groups = await Database
+    async list (options) {
+        const user_id = options.user_id
+        const name = options.name
+        const type = options.type
+
+        var groups = Database
             .select(
                 'name',
+                'user_id',
                 'id',
                 'type'
             )
             .from('groups')
             .where('user_id', user_id)
-
-        return groups
+        
+        if (name) {
+            groups = groups
+                .where('name', name)
+        }
+        if (type) {
+            groups = groups
+                .where('type', type)
+        }
+        const found = await groups
+        if (!found) { found = [] }
+        return Promise.resolve(found)
     }
 
-    async create (user_id, name, options) {
-
-        if (!options) { options = {} }
-        var type = options.type || 'Private'
-        var initialize = options.initialize || false
-
-        var newGroup
-        if (user_id && name) {
-            newGroup = await Database
-                .select(
-                    'id',
-                    'name'
-                )
-                .from('groups')
-                .where(user_id, 'like', user_id)
-                .where(name, 'like', name)
-            
-            if (!newGroup) {
-                console.log('create group: ' + name)
-
-                newGroup = await new Group()
-                newGroup.user_id = user_id
-                newGroup.name = name
-                newGroup.type = type
-            
-                newG = await newGroup.save()
-            }
-        } 
-        return newGroup
-    }
 }
 
 module.exports = Group
