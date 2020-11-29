d84004c47338f1d0a6986d89e39ef492f28b8abc
Ran Guin <rguin@iMac.local>
2020-11-29 12:29:18 -0800

    use mdi

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -1,11 +1,13 @@
+import '@mdi/font/css/materialdesignicons.css'
+
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 Vue.use(Vuetify);
 
 export default new Vuetify({
   icons: {
-    iconfont: 'md',
+    iconfont: 'mdiSvg',
   },
 });
